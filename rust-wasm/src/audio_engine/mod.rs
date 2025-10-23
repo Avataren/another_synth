0c2fa50654874cf8b07489bc1f9bf40c29b4bc40
Morten Andersson <morten.andersson@gmail.com>
2025-10-24 00:29:58 +0200

    Add native CPAL demo binary

--- rust-wasm/src/audio_engine/mod.rs
+++ rust-wasm/src/audio_engine/mod.rs
@@ -4,8 +4,5 @@ pub mod wasm;
 #[cfg(feature = "wasm")]
 pub use wasm::*;
 
-#[cfg(not(feature = "wasm"))]
+#[cfg(feature = "native-host")]
 pub mod native;
-
-#[cfg(not(feature = "wasm"))]
-pub use native::*;
